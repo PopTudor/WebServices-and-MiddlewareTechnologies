b3bcea8261f038e839638dab63e94f23c7e60b97
Pop Tudor <tudor08pop@yahoo.com>
2017-03-11 18:42:33 +0200

    Write four unknown chars at the beginning

--- src/com/tudor/pop/Main.java
+++ src/com/tudor/pop/Main.java
@@ -10,26 +10,24 @@ public class Main {
 
         File source = new File(args[0]);
         File out = new File(source.getName().replace(".txt", "_reversed.txt"));
-        Reader reader = new InputStreamReader(new FileInputStream(source));
-        BufferedWriter writer = new BufferedWriter(new FileWriter(out));
+        BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(source)));
         RandomAccessFile randomAccessFile = new RandomAccessFile(out, "rw");
-        randomAccessFile.setLength(source.length());
-        char[] buff = new char[1];
         // keep track of the current position (we're going backwards, so we start at the end)
         long position = source.length();
         // Reader.read will return -1 when it reached the end.
-        while ((reader.read(buff)) > -1) {
+        for (String line = reader.readLine(); line != null; line = reader.readLine()) {
+            StringBuilder stringBuffer = new StringBuilder(line);
+            String reversed = stringBuffer.reverse().toString()+"\n";
+            position -= reversed.length(); // \r\n is only one newline character
             // turn the character into bytes according to the character encoding
-            String s = String.valueOf(buff[0]);
             // go to the proper position in the random access file
-            position--;
             randomAccessFile.seek(position);
 
             // write one or more bytes for the character
-            randomAccessFile.write(s.getBytes());
+            randomAccessFile.writeUTF(reversed);
         }
 
         randomAccessFile.close();
-        writer.close();
+        reader.close();
     }
 }
