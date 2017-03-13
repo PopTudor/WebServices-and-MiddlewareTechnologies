63811f86e079bfcd026a44de583527dcd7dd471b
Pop Tudor <tudor08pop@yahoo.com>
2017-03-13 22:07:28 +0200

    python char by char

--- python/main.py
+++ python/main.py
@@ -1,4 +1,4 @@
-import re, os, sys
+import re, os, sys, os.path
 
 
 def main(argv):
@@ -6,25 +6,25 @@ def main(argv):
         raise ValueError("Trebuie introdus numele fisierului")
 
     src = open(argv[1], 'r')
-    dest = open("python_reversed.txt", 'w', 1)  # 1-buffered
+    dest = open("python_reversed.txt", 'w')
     dest.truncate()
     # mentine pozitia unde o sa scriem datele in fisier, pornind de la coada la cap
     position = os.path.getsize(os.path.abspath(src.name))
-    line = src.readline()
+    character = src.read(1)
     # fals cand nu mai este nimic de citit
-    while line:
-        line = line[::-1]  # reverse line
-
+    while character:
         # mergi la pozitia corecta in fisier
-        position -= len(line)
+        position -= 1
+        if character == '\n':
+            character = ' \r'
+            position-=1
         dest.seek(position)
-
         # scrie rezultatul
-        dest.write(line)
-        line = src.readline()
+        dest.write(character)
+        character = src.read(1)
     src.close()
-    dest.flush()
     dest.close()
+
 
 if __name__ == "__main__":
     main(sys.argv)
