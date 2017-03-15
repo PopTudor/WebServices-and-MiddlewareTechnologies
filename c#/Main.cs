f5e53c67e26316b811715f6311adc3db91259099
Pop Tudor <tudor08pop@yahoo.com>
2017-03-15 23:21:26 +0200

    Citeste octeti cu fread in C#  si genereaza random file names. Rename fisierele test.txt in test.dat

--- c#/Main.cs
+++ c#/Main.cs
@@ -9,7 +9,7 @@ class TestClass
         if (args.Length < 1)
             throw new System.ArgumentException("Trebuie introdus numele fisierului");
         FileStream source = File.OpenRead(args[0]);
-        FileStream dest = File.OpenWrite("C#_reverse.txt");
+        FileStream dest = File.OpenWrite("C#"+new Random().Next(0,100)+"_reverse.txt");
         dest.SetLength(0);
 
         dest.Seek(1,SeekOrigin.Begin);
@@ -18,12 +18,11 @@ class TestClass
         dest.WriteByte(Convert.ToByte('e'));
 
         long position = source.Length;
-        for (int  line = source.ReadByte(); line != -1; line = source.ReadByte())
+        for (int  octet = source.ReadByte(); octet != -1; octet = source.ReadByte())
         {
             position--;
-            if(line=='\n') line = ' ';
             dest.Seek(position, SeekOrigin.Begin);
-            dest.WriteByte((byte)line);
+            dest.WriteByte((byte)octet);
         }
         source.Dispose();
         dest.Dispose();
