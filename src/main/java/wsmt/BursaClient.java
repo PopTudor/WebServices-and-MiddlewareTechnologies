a8c8c879db5670b9309254a9096015e6caf390b3
Pop Tudor <tudor08pop@yahoo.com>
2017-04-17 20:21:44 +0300

    Some testing

--- src/main/java/wsmt/BursaClient.java
+++ src/main/java/wsmt/BursaClient.java
@@ -16,16 +16,16 @@ public class BursaClient {
 		
 		IBursa bursa = (IBursa) factory.create(IBursa.class, url);
 
-//		System.out.println(bursa.cumpara("apple", 5));
-//		System.out.println(bursa.cumpara("google", 3));
+//		System.out.println(bursa.cumpara("apple", 1));
+//		System.out.println(bursa.cumpara("google", 1));
 //		System.out.println(bursa.vinde("apple", 3));
 		System.out.println(bursa.statistica());
 	}
 	
 	public static void main(String[] args) throws MalformedURLException {
 		// Java
-//		new BursaClient("http://localhost:8081/bursa");
-		// PHP
-					new BursaClient("http://localhost:8081/BursaServer.php");
+		new BursaClient("http://localhost:8081/bursa");
+		// PHP merge doar prin XAMP
+//					new BursaClient("http://localhost:8081/BursaServer.php");
 	}
 }
